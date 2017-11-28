96bb70978ab25aa1e2f6211d1ef6b0467050300b
vmartinez40 <vmartinez40@mail.csuchico.edu>
2017-11-27 17:52:24 -0800

    Added 2 class names for map and battleship

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* Victor Martinez's Battleship Game
+Things to do
+1)Greet player 
+2)Have 3 options (Play(Again?), Rules, and Quit)
+3)Display a map of hits you made and if you hit any ships(make into class)
+4)Randomly generate different size ships (1,2,3,4) and if they are vertical or horizontal(make into class)
+5)
+
+
+*/
+
+#include <string>
+#include <iostream>
+#include <filestream>
+#include <ctime>
+
+using namespace std;
+
+//Prototypes 
+
+//Greeting
+void greeting();
+//Display 3 options Play(Again?), Rules, and Quit and return bool (might be a class?)
+bool menuOptions();
+
+int main()
+{
+
+
+
+
+
+
+return 0;
+}
+
+
